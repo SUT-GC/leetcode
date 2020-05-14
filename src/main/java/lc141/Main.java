f8a4ff6b00e2ef27ae6456c2a5d47e5c0a3f5633
SUT-GC <gouchao2008@qq.com>
2020-05-14 23:38:31 +0800

    add lc 141

---
+++ src/main/java/lc141/Main.java
@@ -0,0 +1,21 @@
+package lc141;
+
+import mate.ListNode;
+
+public class Main {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(3);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(0);
+        ListNode node4 = new ListNode(-4);
+
+        node1.setNext(node2);
+//        node2.setNext(node3);
+//        node3.setNext(node4);
+//        node4.setNext(node2);
+
+        Solution solution = new Solution();
+        System.out.println(solution.hasCycle(node1));
+
+    }
+}
