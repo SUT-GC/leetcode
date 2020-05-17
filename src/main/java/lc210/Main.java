db8adc3719e48d9f51346b2b270a15cc9b486abf
SUT-GC <gouchao2008@qq.com>
2020-05-17 22:47:19 +0800

    update

---
+++ src/main/java/lc210/Main.java
@@ -0,0 +1,11 @@
+package lc210;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] r = new int[0][0];
+
+
+        solution.findOrder(2, r);
+    }
+}
