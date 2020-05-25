a7b746738cdea6701ff53af7c924f7ca4f2d37f1
GC <gouchao@soulapp.cn>
2020-05-25 11:47:32 +0800

    add lc146

--- src/main/java/lc146/LRUCache.java
+++ src/main/java/lc146/LRUCache.java
@@ -4,53 +4,90 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LRUCache {
+    public static class Node {
+        public int k;
+        public int v;
+        public Node pre;
+        public Node next;
 
-    int timeCount = 0;
-    int cap = 0;
+        public Node(int k, int v) {
+            this.k = k;
+            this.v = v;
+        }
+    }
 
-    Map<Integer, Integer> cache = null;
-    Map<Integer, Integer> lruMap = new HashMap<>();
-
+    public int capacity;
+    public int size;
+    public Map<Integer, Node> dict;
+    public Node head;
+    public Node tail;
 
     public LRUCache(int capacity) {
-        this.cache = new HashMap<>(capacity);
-        this.lruMap = new HashMap<>(capacity);
-        this.cap = capacity;
+        this.capacity = capacity;
+        this.size = 0;
+        this.dict = new HashMap<>();
+        this.head = new Node(-1000, -1000);
+        this.tail = new Node(-1001, -1001);
     }
 
     public int get(int key) {
-        lruCount(key);
-        return this.cache.getOrDefault(key, -1);
+        Node node = dict.get(key);
+        if (node == null) {
+            // 如果cache中不存在，返回-1
+            return -1;
+        } else {
+            // 否则将node从原来位置删除，并且插入到链表头
+            this.delete(node);
+            this.insert(node);
+            return node.v;
+        }
     }
+
 
     public void put(int key, int value) {
-        this.cache.put(key, value);
-        lruCount(key);
-        dieOut();
+        Node node = dict.get(key);
+
+        if (node == null) {
+            node = new Node(key, value);
+            // 如果该节点不在cache中
+            // 将这个节点插入到cache中
+            this.insert(node);
+        } else {
+            // 如果节点在cache中
+            node.v = value;
+            // 将这个节点从原来位置删除，并且插入到cache中
+            this.delete(node);
+            this.insert(node);
+        }
     }
 
-    public void lruCount(int key) {
-        timeCount++;
-        lruMap.put(key, timeCount);
+    private void insert(Node node) {
+        if (this.capacity == this.size) {
+            this.delete(this.tail.pre);
+        }
+
+        if (this.head.next == null) {
+            this.head.next = node;
+            node.pre = this.head;
+            this.tail.pre = node;
+            node.next = this.tail;
+        } else {
+            node.next = this.head.next;
+            this.head.next.pre = node;
+            this.head.next = node;
+            node.pre = this.head;
+        }
+
+        this.size++;
+
+        this.dict.put(node.k, node);
     }
 
-    public void dieOut() {
-        if (this.cache.size() <= this.cap) {
-            return;
-        }
+    private void delete(Node node) {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+        this.size--;
 
-        int minCount = Integer.MAX_VALUE;
-        Integer minKey = null;
-        for (Map.Entry<Integer, Integer> integerIntegerEntry : this.lruMap.entrySet()) {
-            if (integerIntegerEntry.getValue() < minCount) {
-                minCount = integerIntegerEntry.getValue();
-                minKey = integerIntegerEntry.getKey();
-            }
-        }
-
-        if (minKey != null) {
-            this.cache.remove(minKey);
-            this.lruMap.remove(minKey);
-        }
+        this.dict.remove(node.k);
     }
 }
---
+++ src/main/java/lc146/LRUCache2.java
@@ -0,0 +1,56 @@
+package lc146;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRUCache2 {
+
+    int timeCount = 0;
+    int cap = 0;
+
+    Map<Integer, Integer> cache = null;
+    Map<Integer, Integer> lruMap = new HashMap<>();
+
+
+    public LRUCache2(int capacity) {
+        this.cache = new HashMap<>(capacity);
+        this.lruMap = new HashMap<>(capacity);
+        this.cap = capacity;
+    }
+
+    public int get(int key) {
+        lruCount(key);
+        return this.cache.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        this.cache.put(key, value);
+        lruCount(key);
+        dieOut();
+    }
+
+    public void lruCount(int key) {
+        timeCount++;
+        lruMap.put(key, timeCount);
+    }
+
+    public void dieOut() {
+        if (this.cache.size() <= this.cap) {
+            return;
+        }
+
+        int minCount = Integer.MAX_VALUE;
+        Integer minKey = null;
+        for (Map.Entry<Integer, Integer> integerIntegerEntry : this.lruMap.entrySet()) {
+            if (integerIntegerEntry.getValue() < minCount) {
+                minCount = integerIntegerEntry.getValue();
+                minKey = integerIntegerEntry.getKey();
+            }
+        }
+
+        if (minKey != null) {
+            this.cache.remove(minKey);
+            this.lruMap.remove(minKey);
+        }
+    }
+}
