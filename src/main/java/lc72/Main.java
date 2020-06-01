d385e9cd3ecc7123228e4ec6d971d8cf40bf2a82
GC <gouchao@soulapp.cn>
2020-06-01 11:09:02 +0800

    add lc72

---
+++ src/main/java/lc72/Main.java
@@ -0,0 +1,8 @@
+package lc72;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.minDistance("abc", "def"));
+    }
+}
---
+++ src/main/java/lc72/Solution.java
@@ -0,0 +1,30 @@
+package lc72;
+
+import java.util.Optional;
+
+public class Solution {
+    /**
+     * 1 + min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])
+     */
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i < word1.length() + 1; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j < word2.length() + 1; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i < word1.length() + 1; i++) {
+            for (int j = 1; j < word2.length() + 1; j++) {
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(Math.min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[word1.length()][word2.length()];
+    }
+}
