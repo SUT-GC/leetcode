2a098ca67b439bada155c7166acb68a93fdc0756
寂兮 <jixi@pinduoduo.com>
2020-11-02 11:25:47 +0800

    add lc 349

---
+++ src/main/java/lc349/Solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        if not nums1 or not nums2:
+            return []
+
+        return list(set(nums1).intersection(set(nums2)))
