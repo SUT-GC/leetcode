b18bde7eefd2853e2b821903f3f64654a1c3cc03
寂兮 <jixi@pinduoduo.com>
2020-12-12 00:43:23 +0800

    update

---
+++ src/main/java/lc649/Main.java
@@ -0,0 +1,9 @@
+package lc649;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.predictPartyVictory("DRRDRDRDRDDRDRDR"));
+    }
+}
